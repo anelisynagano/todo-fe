90ca6185ed5693aa392eb60d8c855ce374761466
anelisynagano <anelisynagano@gmail.com>
2021-07-01 16:07:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [userTodo, setUserToDo] = useState("");
+
+  const fetchTodos = () => {
+    fetch("/todos")
+      .then((res) => res.json())
+      .then((data) => setTodos(data));
+  };
+
+  const handleChange = (e) => {
+    setUserToDo(e.target.value);
+  };
+
+  const handleClick = () => {
+    fetch("/todos", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ description: userTodo }),
+    })
+      .then((res) => res.json())
+      .then((data) => setTodos(data[0], ...todos));
+  };
+
+  useEffect(fetchTodos, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input type='text' value={userTodo} onChange={handleChange} />
+      <button onClick={handleClick}>Add To Do!</button>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo.description}</li>
+        ))}
+      </ul>
     </div>
   );
 }
